27aa8da0435e41a0ce822ba444735db5dbaa88c1
root <root@ip-172-31-83-248.ec2.internal>
2020-11-20 15:19:31 +0000

    webhook check

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">No items yet! Emin misin?!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
